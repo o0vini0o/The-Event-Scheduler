55c0d77dbba56f9184cadb4315607f4e082bb59d
arnaudbrice <mephisteur@gmail.com>
2025-08-06 10:21:14 +0200

    fix: createEvent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 import { Heading } from "./components";
 import { Routes, Route } from "react-router";
 import MainLayout from "./layouts/MainLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
 import Homepage from "./pages/Homepage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -19,16 +19,20 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Homepage />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/logout" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signUp" element={<SignUp />} />
             <Route path="/events/:id" element={<EventDetail />} />
+          </Route>
 
+          <Route>
+            <Route path="/" element={<ProtectedLayout />} />
+            <Route index element={<Homepage />} />
             <Route path="/api/events/new" element={<CreateEvent />} />
+            <Route path="/signUp" element={<SignUp />} />
           </Route>
         </Routes>
       </div>
     </EventContextProvider>
-
   );
 }
 
